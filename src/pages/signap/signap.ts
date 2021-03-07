41381cb838ad5d9f7bcb2ff7328e6c09c8aa1df4
Leidson Oliveira <o.q.leidson@gmail.com>
2021-03-07 14:54:55 -0300

    Criando um FormGroup para controlar o formulario

--- src/pages/signap/signap.ts
+++ src/pages/signap/signap.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { connectableObservableDescriptor } from 'rxjs/observable/ConnectableObservable';
 
@@ -9,7 +10,30 @@ import { connectableObservableDescriptor } from 'rxjs/observable/ConnectableObse
 })
 export class SignapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+      this.formGroup = formBuilder.group({
+        nome: ['Leidson', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+        email: ['leidson@gmail.com', [Validators.required, Validators.email]], 
+        tipo: ['2', [Validators.required]],
+        cpfOrCnpf: ['76566580093',[Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        senha: ['abc', [Validators.required]],
+        logradouro: ['Rua Pj', [Validators.required]],
+        numero: ['55-b',[Validators.required]],
+        complemtno: ['Apt-05', []],
+        bairro: ['Centro', [Validators.required]],
+        cep: ['74.666-000', [Validators.required]],
+        telefone1: ['62-90000-8965',[Validators.required]],
+        telefone2: ['61-95555-0011', []],
+        telefone3: ['', []],
+        estadoId: [null, [Validators.required]],
+        cidadeId: [null, [Validators.required]], 
+      })
   }
 
   signupUser(){
