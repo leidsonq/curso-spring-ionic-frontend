e7811b2b819bc52b561f4a542d027495df23bf1e
Leidson Oliveira <o.q.leidson@gmail.com>
2021-05-17 20:38:58 -0300

    Buscando os enderecos no banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,9 +11,9 @@ export class ClienteService{
     constructor(public http: HttpClient, public storage: StorageService){
     }
 
-    findByEmail(email: string) : Observable<ClienteDTO> {
+    findByEmail(email: string) {
 
-        return this.http.get<ClienteDTO>(
+        return this.http.get(
             `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
